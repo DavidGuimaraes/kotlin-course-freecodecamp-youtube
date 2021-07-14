cbff17a039ff6e0adc3d6b27d5e95f18282e7cd7
David Guimaraes <davguimaraes@pagseguro.com>
2021-07-14 17:40:13 -0300

    04 - Basic Control Flow.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,54 +1,42 @@
 
-/*
-fun main() {
-    print("Hello. World! Again! Fuck!")
-}
-*/
-
-// fun main() = print("Hello. World! Again! Fuck!")
-
-/*
-fun main(args: Array<String>) {
-    print("Hello. World! Again! Fuck!")
-}
-*/
-
-/*
-fun main(args: Array<String>) {
-    print("Hello. World! Again! Fuck! My name is ${args[0]}")
-}
-*/
-
-// fun main(args: Array<String>) = print("Hello. World! Again! Fuck! My name is ${args[0]}")
-
-/*
-class Greet(name: String){
-    init {
-        print("Hello, $name")
-    }
-}
-*/
-
-const val name3: String = "Amanda"        //Top level variable
-var name4: String? = null                 //The '?' operator is the nullable in Kotlin
-/*
-    get() {
-        TODO()
-    }
-    set(value) {}
-*/
 
 fun main(args: Array<String>){
-    val name: String = "David"
-    //name = "Something else"     //We get an error here
+    //var greeting: String? = "Hello"
+    var greeting: String? = null
 
-    var name2: String = "Amanda"
-    name2 = "Peter"             //We won't get an error here as long as we use the 'var' keyword instead of 'val'
-    val name5 = "Sara"          //Variable of type String declared with inference in Kotlin, which is implicit
-    var name6 = null            //In this case, we get a warning and the compiler requests to declare the type explicitly
+    /*
+    if(greeting != null){
+        print("$greeting ${args[0]}")
+    } else{
+        print("Hi, ${args[0]}")
+    }
+    */
 
-    println(name)
-    println(name2)
-    println(name3)
-    println(name4)
+    /*
+    when(greeting){
+        null -> print("Hi, ${args[0]}")
+        else -> print("$greeting ${args[0]}")
+    }
+    */
+
+    // if(greeting == null) print("Hi, ${args[0]}") else print("$greeting ${args[0]}")
+
+    /*
+    greeting = "Hello"
+    var greetingToBePrinted = greeting ?: "Hi"
+    print("$greetingToBePrinted ${args[0]}")
+    */
+
+    /*
+    var greetingToBePrinted = if(greeting != null) greeting else "Hi"
+    println("$greetingToBePrinted ${args[0]}")
+    */
+
+    greeting = "Hello"
+    var greetingToBePrinted = when(greeting){
+        null -> "Hi"
+        else -> greeting
+    }
+
+    println("$greetingToBePrinted ${args[0]}")
 }
