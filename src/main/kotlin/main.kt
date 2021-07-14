1c9240508e499f8de6e12e9e02d4c97ac273cfc6
David Guimaraes <davguimaraes@pagseguro.com>
2021-07-14 16:37:18 -0300

    02 - Hello World.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,32 @@
+
+/*
+fun main() {
+    print("Hello. World! Again! Fuck!")
+}
+*/
+
+// fun main() = print("Hello. World! Again! Fuck!")
+
+/*
 fun main(args: Array<String>) {
-    println("Hello World!")
+    print("Hello. World! Again! Fuck!")
+}
+*/
+
+/*
+fun main(args: Array<String>) {
+    print("Hello. World! Again! Fuck! My name is ${args[0]}")
+}
+*/
+
+// fun main(args: Array<String>) = print("Hello. World! Again! Fuck! My name is ${args[0]}")
+
+class Greet(name: String){
+    init {
+        print("Hello, $name")
+    }
+}
+
+fun main(args: Array<String>){
+    Greet(args[0])
 }
