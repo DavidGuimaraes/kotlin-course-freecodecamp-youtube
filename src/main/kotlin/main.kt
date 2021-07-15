71a83c17d9e04a59345b58c60668258dfed84d78
David Guimaraes <davguimaraes@pagseguro.com>
2021-07-15 01:26:39 -0300

    12 - Named Arguments and Default Parameter Values.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,6 +9,8 @@ fun printMainProgrammingLanguages(vararg programmingLanguages: String) {
     programmingLanguages.forEach { println(it) }
 }
 
+fun greetPerson(greeting: String = "Hola", name: String) = println("$greeting $name")
+
 fun main(args: Array<String>){
     val myMainProgrammingLanguages = listOf("Java", "C#", "Kotlin")
     //printMainProgrammingLanguages() //Throws an error as long as the parameter is the type of List<String>
@@ -18,4 +20,8 @@ fun main(args: Array<String>){
 
     val test = arrayOf("One Things", "Another Thing")
     printMainProgrammingLanguages(*test)    //Using the spread operator (*) we can pass an array as parameter, since the vararg treats everything as array
+
+    greetPerson("Hi", "David")
+    greetPerson(name = "Amanda", greeting = "Hello")
+    greetPerson(name = "Peter")
 }
